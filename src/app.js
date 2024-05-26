855f968874f5e81555bcade659db8a737fa6e426
AKalityuk <AKalityuk@nota.tech>
2024-05-26 20:40:30 +0300

    app-port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = 80;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
